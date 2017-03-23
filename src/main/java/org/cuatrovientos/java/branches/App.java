d2ee58a7f0e4ce822c8b5e622674c455a0544870
Eugene <hola@gmail.com>
2017-03-23 16:30:42 +0100

    First commit of the project.

---
+++ src/main/java/org/cuatrovientos/java/branches/App.java
@@ -0,0 +1,14 @@
+package org.cuatrovientos.java.branches;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Proyecto de prueba" );
+    }
+}

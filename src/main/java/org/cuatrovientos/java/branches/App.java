184132dc5bb9e03f7bbfb7dc0ba8aa16305dbc48
Eugene <hola@gmail.com>
2017-03-23 18:14:20 +0100

    c4

--- src/main/java/org/cuatrovientos/java/branches/App.java
+++ src/main/java/org/cuatrovientos/java/branches/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! Modificado" );
         System.out.println( "Proyecto de prueba" );
     }
 }
